83efd54778edae875959098e27b9ab3f459cd3f3
김승현 <tmdgus0118@gmail.com>
2018-10-01 22:37:42 +0900

    소스 및 보고서 수정

--- assignment1/2012003909_assignment_1.py
+++ assignment1/2012003909_assignment_1.py
@@ -117,9 +117,9 @@ class Maze:
         ret1 = self.ids_sub(self.start, self.key)
         ret2 = self.ids_sub(self.key, self.goal)
         return (ret1[0]+ret2[0], ret1[1]+ret2[1])
-    def astar_sub(self, start: tuple, goal: tuple, h : Callable[[tuple, tuple, int],int]) -> tuple:
+    def pr_bfs_sub(self, start: tuple, goal: tuple, fc : Callable[[tuple, tuple, int],int]) -> tuple:
         # start부터 goal까지 A*함수로 경로를 탐색한다
-        # h -> heuristic function
+        # fc : priority 구하는 함수; Caller 정의
         mz = self.copy()
         q = PriorityQueue()
         
@@ -138,36 +138,38 @@ class Maze:
             if pos == goal:
                 self.applyPath(path, goal)
                 return (time, length)
+            # getDirectionOrder 함수로 도착지점을 찾기 위한 방향을 우선순위별로 얻음
             order = getDirectionOrder(pos, goal)
             for dr in order:
                 mv = mz.move(pos,dr)
                 if mv is not None:
-                    q.put(Maze.AStarNode(h(pos, goal, length+1), (mv, length+1, pos)))
+                    # 함수인자로 전달받은 fc함수를 이용해 Priority Queue의 우선순위를 결정합니다
+                    q.put(Maze.AStarNode(fc(pos, goal, length+1), (mv, length+1, pos)))
             time = time + 1
         return (0,0)
     def gbs(self) -> tuple:
         # Greedy best first search
+        
         h = lambda a,b,c : abs(a[0]-b[0])+abs(a[1]-b[1])
-        r1 = self.astar_sub(self.start, self.key, h)
-        r2 = self.astar_sub(self.key, self.goal, h)
+        r1 = self.pr_bfs_sub(self.start, self.key, h)
+        r2 = self.pr_bfs_sub(self.key, self.goal, h)
         return (r1[0] + r2[0], r1[1]+r2[1])
     def astar(self) -> tuple:
         # heuristic function = 장애물이 하나도 없을 때의 최소거리
         h = lambda a,b,c : abs(a[0]-b[0])+abs(a[1]-b[1])+c
 
-        r1 = self.astar_sub(self.start, self.key, h)
-        r2 = self.astar_sub(self.key, self.goal, h)
+        r1 = self.pr_bfs_sub(self.start, self.key, h)
+        r2 = self.pr_bfs_sub(self.key, self.goal, h)
         return (r1[0] + r2[0], r1[1]+r2[1])
     def dfs(self) -> tuple:
         r1 = self.dfs_sub(self.start,self.key, 100000)
         r2 = self.dfs_sub(self.key,self.goal , 100000)
         return (r1[0]+r2[0], r1[2]+r2[2])
     def bfs(self) -> tuple:
-        # astar에서 h(n) = 0으로 선언하면 uniform cost search와 같다
-        # uniform cost search에서 가중치가 모두 1인경우 BFS와 같다
+        # priority를 안주면; BFS처럼 동작
         h = lambda a,b,c : 0
-        r1 = self.astar_sub(self.start, self.key, h)
-        r2 = self.astar_sub(self.key, self.goal, h)
+        r1 = self.pr_bfs_sub(self.start, self.key, h)
+        r2 = self.pr_bfs_sub(self.key, self.goal, h)
         return (r1[0] + r2[0], r1[1]+r2[1])
     def print(self, stream : io.TextIOWrapper):
         for l in self.map:
@@ -210,19 +212,35 @@ def test(floor:str):
     print('-------------')
 
 def first_floor():
+    # first_floor_input.txt를 읽어 Maze 인스턴스를 생성합니다
+    # 두번째 인자인 lambda 함수로 알고리즘을 실행합니다
+    # 결과를 first_floor_output.txt에 저장합니다
     execute('first', lambda mz : mz.gbs())
     # 여기서 first는 층 ( 이를 이용해 input과 output파일을 가져옵니다 )
     # 뒤의 lambda 함수는 사용할 알고리즘을 정의합니다. mz는 Maze 인스턴스 입니다.
-def second_floor():
+
+def second_floor(): 
+    # second_floor_input.txt를 읽어 Maze 인스턴스를 생성합니다
+    # 두번째 인자인 lambda 함수로 알고리즘을 실행합니다
+    # 결과를 second_floor_output.txt에 저장합니다
     execute('second', lambda mz : mz.gbs())
 
 def third_floor():
+    # third_floor_input.txt를 읽어 Maze 인스턴스를 생성합니다
+    # 두번째 인자인 lambda 함수로 알고리즘을 실행합니다
+    # 결과를 third_floor_output.txt에 저장합니다
     execute('third', lambda mz : mz.gbs())
 
 def fourth_floor():
+    # fourth_floor_input.txt를 읽어 Maze 인스턴스를 생성합니다
+    # 두번째 인자인 lambda 함수로 알고리즘을 실행합니다
+    # 결과를 fourth_floor_output.txt에 저장합니다
     execute('fourth', lambda mz : mz.gbs())
 
 def fifth_floor():
+    # fifth_floor_input.txt를 읽어 Maze 인스턴스를 생성합니다
+    # 두번째 인자인 lambda 함수로 알고리즘을 실행합니다
+    # 결과를 fifth_floor_output.txt에 저장합니다
     execute('fifth', lambda mz : mz.gbs())
 
 mode = 'real'    
